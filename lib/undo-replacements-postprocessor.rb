a2f01cd9b7efe3d6dcdec495fa990f0ffec45deb
Dan Allen <dan.j.allen@gmail.com>
2014-10-17 00:04:00 -0600

    add postprocessor to undo replacements

---
+++ lib/undo-replacements-postprocessor.rb
@@ -0,0 +1,20 @@
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+include ::Asciidoctor
+
+class UndoReplacementsPostprocessor < Extensions::Postprocessor
+  Replacements = [/&#8594;/, /&#8658;/]
+  Originals = ['->', '=>']
+
+  def process document, output
+    Replacements.zip(Originals).each do |replacement, original|
+      output = output.gsub replacement, original
+    end
+    output
+  end
+end
+
+Extensions.register :undo do |document|
+  document.postprocessor UndoReplacementsPostprocessor
+end
