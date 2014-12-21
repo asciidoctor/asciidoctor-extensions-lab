ce5aac14cbe4c34c6526fddd2d35e78cb6a37f44
Guillaume Grossetie <ggrossetie@gmail.com>
2014-12-21 11:15:10 +0100

    Adds size map for tortue emoji

--- lib/emoji-inline-macro/extension.rb
+++ lib/emoji-inline-macro/extension.rb
@@ -6,19 +6,22 @@ class EmojiBlockMacro < Extensions::InlineMacroProcessor
   use_dsl
   named :emoji
 
+  SIZE_MAP = {'1x' => 17, 'lg' => 24, '2x' => 34, '3x' => 50, '4x' => 68, '5x' => 85}
+  SIZE_MAP.default = 24
+
   def process parent, target, attributes
     doc = parent.document
     if doc.attributes['emoji'] == 'tortue'
       slash = (doc.attr? 'htmlsyntax', 'xml') ? '/' : nil
-      size = (attributes.key? 'size') ? attributes['size'] : '24'
+      size = SIZE_MAP[attributes['size']]
       cdn = (attributes.key? 'cdn') ? attributes['cdn'] : 'http://www.tortue.me/emoji/'
       qtarget = %(#{cdn}#{target}.png)
-      %(<img src="#{parent.image_uri qtarget, nil}" height="#{size}" width="#{size}"#{slash}>)
+      %(<img src="#{parent.image_uri qtarget, nil}" height="#{size}px" width="#{size}px"#{slash}>)
     else
       # By default twemoji
       slash = (doc.attr? 'htmlsyntax', 'xml') ? '/' : nil
       size = (attributes.key? 'size') ? attributes['size'] : nil
-      emojiTag = target.tr("_", "-")
+      emojiTag = target.tr('_', '-')
       if size == nil
         %(<i class="twa twa-#{emojiTag}"></i>)
       else
