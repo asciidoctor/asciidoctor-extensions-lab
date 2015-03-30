7327ff727f2d1d32f5b285337903c27dc1f78d57
Dan Allen <dan.j.allen@gmail.com>
2015-03-30 15:39:44 +0200

    add ability to pass attributes to the coalescer script

--- scripts/asciidoc-coalescer.rb
+++ scripts/asciidoc-coalescer.rb
@@ -1,8 +1,26 @@
 # This script coalesces the AsciiDoc source into a single file by resolving all
 # preprocessor directives, in particular include directives.
 
-require 'asciidoctor'
+# TODO
+# - add option to write cli attributes into header of document
+# - add option to write output to file
 
-source_file = ARGV[0]
-doc = Asciidoctor.load_file source_file, safe: :safe, parse: false
-puts doc.reader.read
+require 'asciidoctor'
+require 'optparse'
+
+options = { attributes: [] }
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby asciidoc-coalescer.rb [OPTIONS] FILE'
+  opts.on('-a', '--attribute key[=value]', 'A document attribute to set in the form of key[=value]') do |a|
+    options[:attributes] << a
+  end
+end.parse!
+
+unless (source_file = ARGV.shift)
+  warn 'Please specify an AsciiDoc source file to coalesce.'
+  exit 1
+end
+
+doc = Asciidoctor.load_file source_file, safe: :safe, parse: false, attributes: options[:attributes]
+lines = doc.reader.read.gsub(/^include::(?=.*\[\]$)/m, '\\include::')
+puts lines
