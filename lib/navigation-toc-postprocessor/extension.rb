3840fc1a95248748a2c52e19949ceebc2b029772
Nico Rikken <nico.rikken@mpare.net>
2016-07-10 10:16:04 +0200

    feat: css based improvements

--- lib/navigation-toc-postprocessor/extension.rb
+++ lib/navigation-toc-postprocessor/extension.rb
@@ -1,26 +1,39 @@
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 
 class NavigationTocPostprocessor < Asciidoctor::Extensions::Postprocessor
+
   def process document, output
     content = ""
     if document.attr 'nav-home'
-      content << "<a href=\"#{(document.attr 'nav-home')}\"><i class=\"fa fa-home\" aria-hidden=\"true\"></i></a>&nbsp;"
+      content << "<div id=\"toc-nav-item\"><a href=\"#{(document.attr 'nav-home')}\"><i class=\"fa fa-home\" aria-hidden=\"true\"></i></a></div>"
     end
     if document.attr 'nav-prev'
-      content << "<a href=\"#{(document.attr 'nav-prev')}\"><i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i></a>&nbsp;"
+      content << "<div id=\"toc-nav-item\"><a href=\"#{(document.attr 'nav-prev')}\"><i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i></a></div>"
     end
     if document.attr 'nav-up'
-      content << "<a href=\"#{(document.attr 'nav-up')}\"><i class=\"fa fa-arrow-up\" aria-hidden=\"true\"></i></a>&nbsp;"
+      content << "<div id=\"toc-nav-item\"><a href=\"#{(document.attr 'nav-up')}\"><i class=\"fa fa-arrow-up\" aria-hidden=\"true\"></i></a></div>"
     end
     if document.attr 'nav-down'
-      content << "<a href=\"#{(document.attr 'nav-down')}\"><i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i></a>&nbsp;"
+      content << "<div id=\"toc-nav-item\"><a href=\"#{(document.attr 'nav-down')}\"><i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i></a></div>"
     end
     if document.attr 'nav-next'
-      content << "<a href=\"#{(document.attr 'nav-next')}\"><i class=\"fa fa-arrow-next\" aria-hidden=\"true\"></i></a>&nbsp;"
+      content << "<div id=\"toc-nav-item\"><a href=\"#{(document.attr 'nav-next')}\"><i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i></a></div>"
     end
+
+    css_replacement = %(<style>
+/*navigation-toc-postprocessor asciidoctor extension*/
+div .toc2 #toc-nav{
+  font-size: 1.6em;
+   display: flex;
+   justify-content: space-around;
+}
+</style>
+</head>)
+
     if document.basebackend? 'html'
-      replacement = %(\\1<div id="toc-nav">\n#{content}</div>)
-      output = output.sub(/(<div id="toctitle".*?<\/div>)/m, replacement)
+      nav_replacement = %(\\1<div id="toc-nav">\n#{content}</div>)
+      output = output.sub(/(<div id="toctitle".*?<\/div>)/m, nav_replacement)
+      output = output.sub(/<\/head>/m, css_replacement)
     end
     output
   end
