946ed14b758f2c2af7fcc2201781e81d4f369311
Dan Allen <dan.j.allen@gmail.com>
2016-10-19 12:40:07 -0600

    rewrite result block toggle extension as a docinfo processor

--- lib/view-result-postprocessor.rb
+++
@@ -1,5 +0,0 @@
-RUBY_ENGINE == 'opal' ? (require 'view-result-postprocessor/extension') : (require_relative 'view-result-postprocessor/extension')
-
-Asciidoctor::Extensions.register do
-  postprocessor ViewResultPostprocessor if @document.basebackend? 'html'
-end
