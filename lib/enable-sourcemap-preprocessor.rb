eca8ab667d9584180d21e02d842de0b770dc8a87
Dan Allen <dan.j.allen@gmail.com>
2016-12-11 00:15:03 -0700

    make the sourcemap attribute on Document writable, if necessary, then use it

--- lib/enable-sourcemap-preprocessor.rb
+++ lib/enable-sourcemap-preprocessor.rb
@@ -1,12 +1,16 @@
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 
+class Asciidoctor::Document
+  attr_writer :sourcemap unless method_defined? :sourcemap=
+end
+
 # A preprocessor that enables the sourcemap feature if not already enabled via
 # the API. Useful to use in combination with other extensions that rely on the
 # source location information.
 Asciidoctor::Extensions.register do
   preprocessor do
     process do |doc, reader|
-      doc.instance_variable_set :@sourcemap, true
+      doc.sourcemap = true
       nil
     end
   end
