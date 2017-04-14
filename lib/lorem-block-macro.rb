16488fe12f46a0a02b48acf73d2df43dd2e604c6
Dan Allen <dan.j.allen@gmail.com>
2017-04-14 03:18:01 -0600

    fix attribute configuration for lorem block macro

--- lib/lorem-block-macro.rb
+++ lib/lorem-block-macro.rb
@@ -6,11 +6,12 @@ require 'asciidoctor/extensions'
 Asciidoctor::Extensions.register do
   block_macro :lorem do
     name_positional_attributes 'size'
+    default_attrs 'size' => 1
     process do |parent, target, attrs|
       lorem = Middleman::Extensions::Lorem::LoremObject
       method = target.to_sym
       if lorem.respond_to? method
-        content = lorem.send(method, attrs.fetch('length', 1).to_i.abs)
+        content = lorem.send(method, attrs['size'].to_i.abs)
         create_paragraph parent, content, {}
       else
         warn 'Unknown lorem target for lorem block macro'
