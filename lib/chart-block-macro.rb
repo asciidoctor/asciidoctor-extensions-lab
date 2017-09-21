2862bcc6346bb1236895502d4cabb193febe510c
Dan Allen <dan.j.allen@gmail.com>
2017-09-20 23:51:26 -0600

    add reference to jsgraphs.com in chart block macro

--- lib/chart-block-macro.rb
+++ lib/chart-block-macro.rb
@@ -1,5 +1,7 @@
 RUBY_ENGINE == 'opal' ? (require 'chart-block-macro/extension') : (require_relative 'chart-block-macro/extension')
 
+# see http://www.jsgraphs.com/ for a comparison of JavaScript charting libraries
+
 Extensions.register do
   if document.basebackend? 'html'
     block_macro ChartBlockMacro
