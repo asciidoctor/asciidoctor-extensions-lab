9f0b82922c40792ddac504708d203ee2c956e081
Daniel Mulholland <dan.mulholland@gmail.com>
2017-09-23 07:13:26 +1200

    feat: Added preprocessor for including data like commit hash, branch or tags through prenamed attributes

--- lib/git-metadata-preprocessor/extension.rb
+++ lib/git-metadata-preprocessor/extension.rb
@@ -1,52 +1,38 @@
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 
-require 'git'
-#require 'logger'
+require 'rugged'
 
 class GitMetadataPreprocessor < Asciidoctor::Extensions::Preprocessor
   def process document, reader
-    #g = Git.open(document.attributes['git-directory']) # :log => Logger.new(STDOUT)
-    output=`git rev-parse --is-inside-work-tree`
-    result=$?.success?
-    puts(output.strip().class)
-    puts(result.class)
-    if result == true and output.strip() == 'true'
-      mypath = `git rev-parse --show-toplevel`
-      mypath = mypath.gsub('/','\\') #.gsub(/^\//,'//')
-      puts(mypath)
-      g = Git.open(mypath)
-    else
-      abort("She cannot take any more of this, Captain!")
 
+    begin
+      repo = Rugged::Repository.discover(".")
+    rescue
+      $stderr.puts('Failed to find repository, git-metadata extension terminating')
       exit
     end
-    # git
-    # true then
-    # git rev-parse --show-toplevel
 
-    document.sourcemap = true
-    $stderr.puts(document.attributes) # outputs environment, not document attributes
-    # how do I get the document attributes?
+    head = repo.head
 
-    # not like this...
-    header_attr_names = (document.instance_variable_get :@attributes_modified).to_a
-    header_attr_names.each {|k| $stderr.puts(k) }
+    document.attributes['git-metadata-sha'] = head.target_id
+    document.attributes['git-metadata-sha-short'] = head.target_id[0,7]
+    document.attributes['git-metadata-author-name'] = head.target.author[:name]
+    document.attributes['git-metadata-author-email'] = head.target.author[:email]
+    document.attributes['git-metadata-date-mdy'] = head.target.time.strftime("%m-%d-%y")
+    document.attributes['git-metadata-date-dmy'] = head.target.time.strftime("%d-%m-%y")
+    document.attributes['git-metadata-commit-message'] = head.target.message
+    document.attributes['git-metadata-branch'] = repo.branches[head.name].name
 
-    head = g.object('HEAD')
-    document.attributes['git-metadata-sha'] = head.sha
-    document.attributes['git-metadata-sha-short'] = head.sha[0,7]
-    document.attributes['git-metadata-author-name'] = head.author.name
-    document.attributes['git-metadata-author-email'] = head.author.name
-    document.attributes['git-metadata-date-mdy'] = head.date.strftime("%m-%d-%y")
-    document.attributes['git-metadata-date-dmy'] = head.date.strftime("%d-%m-%y")
-    document.attributes['git-metadata-commit-message'] = head.message
-    document.attributes['git-metadata-branch'] = g.current_branch
-    #document.attributes['git-metadata-tag'] = g.describe(head,{:tags => true})
+    tags = []
+    a = Rugged::TagCollection.new(repo)
+    a.each do |tag|
+      if tag.target_id == head.target_id
+        tags << tag.name
+      end
+    end
+
+    document.attributes['git-metadata-tag'] = tags.join(', ')
 
     reader
   end
-end
-
-Asciidoctor::Extensions.register do
-  preprocessor GitMetadataPreprocessor
 end
