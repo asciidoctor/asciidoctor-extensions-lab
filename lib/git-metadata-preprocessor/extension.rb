eda05bf31b185ef9d61412c4e015aa868d36f41a
Dan Allen <dan.j.allen@gmail.com>
2017-10-04 00:44:57 -0600

    refactor code in git-metadata-preprocessor

--- lib/git-metadata-preprocessor/extension.rb
+++ lib/git-metadata-preprocessor/extension.rb
@@ -7,57 +7,51 @@ class GitMetadataPreprocessor < Asciidoctor::Extensions::Preprocessor
   def process document, reader
 
     begin
-      repo = Rugged::Repository.discover(".")
+      repo = Rugged::Repository.discover('.')
     rescue
       $stderr.puts('Failed to find repository, git-metadata extension terminating')
       return
     end
 
-    if repo.empty? or repo.bare?
+    if repo.empty? || repo.bare?
       $stderr.puts('Repository is empty or bare repository, git-metadata extension terminating')
       return
     end
 
+    doc_attrs = document.attributes
     head = repo.head
 
-    document.attributes['git-metadata-sha'] = head.target_id
-    document.attributes['git-metadata-sha-short'] = head.target_id[0,7]
-    document.attributes['git-metadata-author-name'] = head.target.author[:name]
-    document.attributes['git-metadata-author-email'] = head.target.author[:email]
-    document.attributes['git-metadata-date'] = head.target.time.strftime("%Y-%m-%d")
-    document.attributes['git-metadata-time'] = head.target.time.strftime("%H:%M:%S")
-    document.attributes['git-metadata-timezone'] = head.target.time.strftime("%Z")
-    document.attributes['git-metadata-commit-message'] = head.target.message
+    doc_attrs['git-metadata-sha'] = head.target_id
+    doc_attrs['git-metadata-sha-short'] = head.target_id.slice 0, 7
+    doc_attrs['git-metadata-author-name'] = head.target.author[:name]
+    doc_attrs['git-metadata-author-email'] = head.target.author[:email]
+    doc_attrs['git-metadata-date'] = head.target.time.strftime '%Y-%m-%d'
+    doc_attrs['git-metadata-time'] = head.target.time.strftime '%H:%M:%S'
+    doc_attrs['git-metadata-timezone'] = head.target.time.strftime '%Z'
+    doc_attrs['git-metadata-commit-message'] = head.target.message
 
     if repo.head_detached?
-      document.attributes['git-metadata-branch'] = 'HEAD detached'
+      doc_attrs['git-metadata-branch'] = 'HEAD detached'
     elsif repo.head_unborn?
-      document.attributes['git-metadata-branch'] = 'HEAD unborn'
+      doc_attrs['git-metadata-branch'] = 'HEAD unborn'
     else
-      document.attributes['git-metadata-branch'] = repo.branches[head.name].name
+      doc_attrs['git-metadata-branch'] = repo.branches[head.name].name
     end
 
-    tags = []
-    t = Rugged::TagCollection.new(repo)
-    t.each do |tag|
-      if (tag.target_id == head.target_id) or \
-         (tag.annotated? and (tag.annotation.target_id == head.target_id))
-        tags << tag.name
-      end
-    end
-    if tags != []
-      document.attributes['git-metadata-tag'] = tags.join(', ')
-    end
+    tags = repo.tags
+        .select {|t| t.target_id == head.target_id || (t.annotated? && t.annotation.target_id == head.target_id) }
+        .map(&:name)
+    doc_attrs['git-metadata-tag'] = tags * ', ' unless tags.empty?
 
     file_location = Pathname.new Dir.pwd
-    repo_location = Pathname.new File.join(repo.path, '..') # repo.path uses the .git directory
-    document.attributes['git-metadata-relative-path'] = repo_location.relative_path_from file_location
-    document.attributes['git-metadata-repo-path'] = repo_location.realpath
+    repo_location = Pathname.new File.dirname(repo.path) # repo.path uses the .git directory
+    doc_attrs['git-metadata-relative-path'] = repo_location.relative_path_from file_location
+    doc_attrs['git-metadata-repo-path'] = repo_location.realpath
 
     if repo.remotes['origin']
-      document.attributes['git-metadata-remotes-origin'] = repo.remotes['origin'].url
+      doc_attrs['git-metadata-remotes-origin'] = repo.remotes['origin'].url
     end
 
-    reader
+    nil
   end
 end
