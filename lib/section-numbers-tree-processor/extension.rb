b8a5a24439987d6155fac55e05c43d0b5b2a39a8
andrewcarver <andy.carver@yahoo.com>
2018-05-01 16:00:51 -0600

    Added support for different number-styles ('A', 'a', '1') at different levels; namespaced (sorta) global vars

--- lib/section-numbers-tree-processor/extension.rb
+++ lib/section-numbers-tree-processor/extension.rb
@@ -4,16 +4,18 @@ include Asciidoctor
 
 # Mess with these values to your heart's content:
 
-$punct = '.'         # punctuation between levels' numbers: e.g., '.' yields '1.3.2'
-$nr_suffix = '. '    # punctuation after the number and before the chapter's/section's title
-$chap_nums = true    # enables/disables chapter-numbering
-$chap_nr_prefix = 'Chapter '
-$sec_nr_prefix = ''
+$punct_sntp = '.'         # punctuation between levels' numbers: e.g., '.' yields '1.3.2'
+$nr_suffix_sntp = '. '    # punctuation after the number and before the chapter's/section's title
+$chap_nums_sntp = true    # enables/disables chapter-numbering
+$chap_nr_prefix_sntp = 'Chapter '
+$sec_nr_prefix_sntp = ''
+# The style you want at each level (first level is chapters; each level's style must be 'A', 'a', or '1'):
+$styles_arr_sntp = [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ]
 
 # DON'T mess with these values:
 
-$levels_arr = [ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' ]
-$level_previous = 0  # pertinent only where someone has, in their sequence, wrongly jumped two rather than only one level deeper
+$levels_arr_sntp = [ '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' ]
+$level_previous_sntp = 0  # pertinent only where someone has, in their sequence, wrongly jumped two rather than only one level deeper
 
 class SectionNumbersTreeProcessor < Extensions::TreeProcessor
   def process document
@@ -36,29 +38,37 @@ class SectionNumbersTreeProcessor < Extensions::TreeProcessor
   
   def concat_sec_nr block, levels, dtype
     if block.numbered && (defined? block.level) && (!(levels) || block.level.to_i <= levels.to_i)
-      if block.level.to_i > $levels_arr.length
-        for i in ($levels_arr.length)..block.level.to_i
-          $levels_arr.push '0'
+      if block.level.to_i > $levels_arr_sntp.length
+        for i in ($levels_arr_sntp.length)..block.level.to_i
+          $levels_arr_sntp.push '0'
         end
       end
-      if block.level.to_i > $level_previous + 1
-        for i in ($level_previous + 1)..block.level.to_i
-          $levels_arr[i - 1] = '1'
+      if block.level.to_i > $level_previous_sntp + 1
+        for i in ($level_previous_sntp + 1)..block.level.to_i
+          $levels_arr_sntp[i - 1] = $styles_arr_sntp[i - 1]
         end
       end
-      $level_previous = block.level.to_i
-      $levels_arr[block.level.to_i - 1] = block.number.to_s
-      pref = (dtype && dtype == 'book' && block.level.to_i == 1) ? $chap_nr_prefix : \
-          ( block.level.to_i > 0 ? $sec_nr_prefix : '')
-      str = pref + $levels_arr[0]
+      $level_previous_sntp = block.level.to_i
+      $levels_arr_sntp[block.level.to_i - 1] = convert_block_nr block.number, block.level.to_i
+      pref = (dtype && dtype == 'book' && block.level.to_i == 1) ? $chap_nr_prefix_sntp : \
+          ( block.level.to_i > 0 ? $sec_nr_prefix_sntp : '')
+      str = pref + $levels_arr_sntp[0]
       for i in 1..(block.level.to_i - 1)
-        str += "#{$punct}#{$levels_arr[i]}"
+        str += "#{$punct_sntp}#{$levels_arr_sntp[i]}"
       end
-      str += $nr_suffix
-      ($chap_nums || !dtype || dtype != 'book' || block.level.to_i != 1) ? str : ''
+      str += $nr_suffix_sntp
+      ($chap_nums_sntp || !dtype || dtype != 'book' || block.level.to_i != 1) ? str : ''
     else
       nil
     end
+  end
+  
+  def convert_block_nr nr, level
+    my_char = String.new($styles_arr_sntp[level - 1])
+    for i in 1..(nr - 1)
+      my_char.succ!
+    end
+    return my_char
   end
 end
 
