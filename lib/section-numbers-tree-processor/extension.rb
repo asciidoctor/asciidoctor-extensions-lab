ea64fb95b15e87bc16703e52913c006569bf5b38
andrewcarver <andy.carver@yahoo.com>
2018-05-01 19:35:03 -0600

    Added control over whether, in a book, sec-label nrs include chapter's nr

--- lib/section-numbers-tree-processor/extension.rb
+++ lib/section-numbers-tree-processor/extension.rb
@@ -6,7 +6,8 @@ include Asciidoctor
 
 $punct_sntp = '.'         # punctuation between levels' numbers: e.g., '.' yields '1.3.2'
 $nr_suffix_sntp = '. '    # punctuation after the number and before the chapter's/section's title
-$chap_nums_sntp = true    # enables/disables chapter-numbering
+$chap_nums_sntp = true    # enables/disables chapter-labeling
+$chap_nr_in_sec_nr_sntp = true   # in book, starts sec-labels' nrs with chapter's number (whether or not chap-labeling enabled)
 $chap_nr_prefix_sntp = 'Chapter '
 $sec_nr_prefix_sntp = ''
 # The style you want at each level (first level is chapters; each level's style must be 'A', 'a', or '1'; add more if you need):
@@ -52,8 +53,9 @@ class SectionNumbersTreeProcessor < Extensions::TreeProcessor
       $levels_arr_sntp[block.level.to_i - 1] = convert_block_nr block.number, block.level.to_i
       pref = (dtype && dtype == 'book' && block.level.to_i == 1) ? $chap_nr_prefix_sntp : \
           ( block.level.to_i > 0 ? $sec_nr_prefix_sntp : '')
-      str = pref + $levels_arr_sntp[0]
-      for i in 1..(block.level.to_i - 1)
+      level_to_start = (dtype && dtype == 'book' && block.level.to_i > 1 && !$chap_nr_in_sec_nr_sntp) ? 2 : 1
+      str = pref + $levels_arr_sntp[level_to_start - 1]
+      for i in level_to_start..(block.level.to_i - 1)
         str += "#{$punct_sntp}#{$levels_arr_sntp[i]}"
       end
       str += $nr_suffix_sntp
