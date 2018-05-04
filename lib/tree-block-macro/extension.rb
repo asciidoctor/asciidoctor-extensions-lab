3bf15d8edc340066c1fe06dcd0b589aed5798216
Martin Mauch <martin.mauch@gmail.com>
2018-05-04 10:17:44 +0200

    Use pure Ruby to find files in TreeBlockMacro

--- lib/tree-block-macro/extension.rb
+++ lib/tree-block-macro/extension.rb
@@ -1,4 +1,5 @@
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+require 'pathname'
 
 include Asciidoctor
 
@@ -22,22 +23,43 @@ class TreeBlockMacro < Asciidoctor::Extensions::BlockMacroProcessor
     levelarg = maxdepth ? %(-L #{maxdepth}) : nil
     sizearg = (attrs.has_key? 'size-option') ? '-sh --du' : nil
     dirarg = (attrs.has_key? 'dir-option') ? '-d' : nil
-    # TODO use https://github.com/tokiro/tree.rb instead
     # TODO could also output using plantuml
-    cmd = %(tree --charset unicode --noreport --dirsfirst -F #{levelarg} #{sizearg} #{dirarg} #{target}).split(/ +/) * ' '
-    tree = %x(#{cmd}).lines.entries[1..-1].join
     subs = if sizearg
-      tree = tree.gsub(/\[(.*?)\]  (?=\S)/, '[.gray]#[\1]# ')
       [:specialcharacters, :quotes, :macros]
     else
       [:specialcharacters, :macros]
     end
-    tree = tree.gsub(/[|`]-- (.*)\/$/, 'icon:folder-open[role=lime] \1/')
-        .gsub(/[|`]-- (.*)\*$/, 'icon:gears[] \1')
-        .gsub(/[|`]-- (.*\.adoc)$/, 'icon:file-text[role=silver] \1')
-        .gsub(/[|`]-- /, 'icon:file[role=silver] ')
-        .tr('|', ' ')
+    files_with_sizes = Dir["#{target}/**/*"].map {|f| [f + (File.directory?(f) ? '/' : ''), File.size(f)] }
+    lines = files_with_sizes.map do |file, file_size|
+      p = Pathname(file)
+      final_size = p.directory? ? files_with_sizes.select {|f, s| f.start_with?(file)}.map(&:last).sum : file_size
+      size_string = sizearg ? "[.gray]#[#{filesize(final_size)}]#" : ''
+      "    " * (p.each_filename.count - 1) + "icon:#{icon(p)} #{p.basename}#{p.directory? ? '/' : ''}"
+    end
     #warn tree
-    create_listing_block parent, tree, attrs, subs: subs
+    create_listing_block parent, lines.join("\n"), attrs, subs: subs
+  end
+
+  private
+  def filesize(size)
+    units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'Pib', 'EiB']
+
+    return '0.0 B' if size == 0
+    exp = (Math.log(size) / Math.log(1024)).to_i
+    exp = 6 if exp > 6
+
+    '%.1f %s' % [size.to_f / 1024 ** exp, units[exp]]
+  end
+
+  def icon(p)
+    if p.directory?
+      'folder-open[role=lime]'
+    elsif p.executable?
+      'gears[]'
+    elsif p.extname == 'adoc'
+      'file-text[role=silver]'
+    else
+      'file[role=silver]'
+    end
   end
 end
