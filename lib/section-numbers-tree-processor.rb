b4b34d3d6ba7b8f1fa0031ba581632750d4253ed
andrewcarver <andy.carver@yahoo.com>
2018-05-15 14:20:26 -0600

    normalized some whitespace

--- lib/section-numbers-tree-processor.rb
+++ lib/section-numbers-tree-processor.rb
@@ -9,14 +9,14 @@ I would strongly advise that, after using this extension in your to-DocBook conv
 If using asciidoctor-fopub, I'd set the XSL parameter 'section.autolabel' to 0 also. Here's why, if you're interested:
 
     In the standard DocBook XSL stylesheets, chapter.autolabel is 1 by default, whereas section.autolabel is 0 by default.
- 
+
     But in asciidoctor-fopub, whether these are 1 or 0 by default DEPENDS, on whether the :sectnums: attribute was unset in your AsciiDoc header metadata:
-	
+
     they default to 1 IF the <?asciidoc-numbered?> XML processing-instruction is present in the DocBook XML file; and 
     that processing-instruction will be present UNLESS :sectnums: was unset in your AsciiDoc document's header metadata.
-   
+
 (For how to pass XSL stylesheet parameters when using asciidoctor-fopub, please see https://github.com/asciidoctor/asciidoctor-fopub#custom-xsl-parameters.)
-   
+
 Please note that there are several configuration variables at the top of the accompanying extension.rb file, that you can set to control how the extension does the numbering.
 
 =end
