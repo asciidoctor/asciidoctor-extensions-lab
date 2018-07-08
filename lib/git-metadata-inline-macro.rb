72d1d7b28014c7e464866ab671a5428ed7436555
Daniel Mulholland <dan.mulholland@gmail.com>
2018-07-08 15:20:44 +1200

    Make macro independent of converter

--- lib/git-metadata-inline-macro.rb
+++ lib/git-metadata-inline-macro.rb
@@ -1,7 +1,5 @@
 RUBY_ENGINE == 'opal' ? (require 'git-metadata-inline-macro/extension') : (require_relative 'git-metadata-inline-macro/extension')
 
 Asciidoctor::Extensions.register do
-  if @document.basebackend? 'html'
     inline_macro GitMetadataInlineMacro
-  end
 end
