acee5189493e494bf236f3f02ef37e6e6fcc6ad0
Dan Allen <dan.j.allen@gmail.com>
2018-10-25 22:15:39 -0600

    iterate on find_by return value instead of using filter block in fold lines tree processor

--- lib/fold-lines-tree-processor.rb
+++ lib/fold-lines-tree-processor.rb
@@ -2,7 +2,7 @@ Asciidoctor::Extensions.register do
   # Replaces newlines (i.e., line feeds) in paragraphs with a single space.
   tree_processor do
     process do |doc|
-      doc.find_by context: :paragraph do |para|
+      (doc.find_by context: :paragraph).each do |para|
         para.lines = [para.lines * ' ']
       end
     end
