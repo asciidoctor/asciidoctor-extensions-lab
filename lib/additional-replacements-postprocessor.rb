f1f447397f5624b66b5459a96c12fce503529f75
Craig Jones <craigjones@chiefsimplicityofficer.com>
2019-01-31 21:32:24 -0800

    An AsciiDoctor extension that provides certain additional replacements (of the sort that would have been set in the [Replacements] section of asciidoc.conf when using AsciiDoc Python). This first cut includes (o) for the degree symbol, 1/2, 1/4, 3/4, and a dozen fairly common words that contain acutes, umlauts, and/or cedillas (entree, cliche, soupcon, naive, etc.)

---
+++ lib/additional-replacements-postprocessor.rb
@@ -0,0 +1,36 @@
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+
+include Asciidoctor
+
+class AdditionalReplacementsPostprocessor < Extensions::Postprocessor
+  MathSymbolPatterns = [/(?<!\\)\(o\)/, /(?<![\\0-9])1\/2(?![0-9])/, /(?<![\\0-9])1\/4(?![0-9])/, /(?<![\\0-9])3\/4(?![0-9])/]
+  MathSymbols = ['&#176;', '&#189;', '&#188;', '&#190;']
+
+  MathSymbolPatternsEscaped = [/\\\(o\)/, /\\1\/2/, /\\1\/4/, /\\3\/4/]
+  MathSymbolsEscaped = ['(o)', '1/2', '1/4', '3/4']
+
+  AcuteWordPatterns = [/\bSaute/, /\bsaute/, /\bDecor\b/, /\bdecor\b/, /\bCliche/, /\bcliche/, /\bEntree/, /\bentree/, /\bTouche\b/, /\btouche\b/]
+  AcuteWords = ['Saut&#233;', 'saut&#233;', 'D&#233;cor', 'd&#233;cor', 'Clich&#233;', 'clich&#233;', 'Entr&#233;e', 'entr&#233;e', 'Touch&#233;', 'touch&#233;']
+
+  CedillaWordPatterns = [/\bFacade/, /\bfacade/, /\bSoupcon/, /\bsoupcon/]
+  CedillaWords = ['Fa&#231;ade', 'fa&#231;ade', 'Soup&#231;on', 'soup&#231;on']
+
+  UmlautWordPatterns = [/\bNaive/, /\bnaive/, /\bNaivite/, /\bnaivite/]
+  UmlautWords = ['Na&#239;ve', 'na&#239;ve', 'Na&#239;vit&#233;', 'na&#239;vit&#233;']
+
+  CombinedPatterns = MathSymbolPatterns + MathSymbolPatternsEscaped + AcuteWordPatterns + CedillaWordPatterns + UmlautWordPatterns
+  CombinedReplacements = MathSymbols + MathSymbolsEscaped + AcuteWords + CedillaWords + UmlautWords
+
+  def process document, output
+    CombinedPatterns.zip(CombinedReplacements).each do |pattern, replacement|
+      output = output.gsub pattern, replacement
+    end
+    output
+  end
+end
+
+Extensions.register :additionalreplacements do |document|
+  document.postprocessor AdditionalReplacementsPostprocessor
+end
+
+
