9214668b3d614d4974549a5c197d477d3e2e56cb
andrewcarver <andy.carver@yahoo.com>
2019-08-04 15:40:32 -0600

    Support AsciiDoc appendices
    The previous code broke when hitting an AsciiDoc appendix (for which the supplied block.number turns out to be a letter: A, B, ...). This improvement not only fixes that glitch, but also makes appendix-numbering as flexible as it makes the chapter- and section-numbering.

--- lib/section-numbers-tree-processor/extension.rb
+++ lib/section-numbers-tree-processor/extension.rb
@@ -13,9 +13,11 @@ $part_nr_in_chap_nr_sntp = false  # in book, starts chap.-labels' nrs with part'
 $part_nr_in_sec_nr_sntp = true # comes into play only if $part_nr_in_chap_nr_sntp = true AND $chap_nr_in_sec_nr_sntp = true
 $part_nr_prefix_sntp = 'Part '
 $chap_nr_prefix_sntp = 'Chapter '
+$appx_nr_prefix_sntp = 'Appendix '
 $sec_nr_prefix_sntp = ''
 # The style you want at each level (level 0 is PARTS, 1 CHAPS; set styles to 'A','a','I','i', or '1'; add levels if you need to):
 $styles_arr_sntp = [ 'I', '1', '1', '1', '1', '1', '1', '1', '1', '1' ]
+$styles_appendix_sntp = [ 'I', 'A', '1', '1', '1', '1', '1', '1', '1', '1' ]  # Styles for appx's; level 0 has no effect here
 
 # DON'T mess with these values:
 
@@ -43,23 +45,28 @@ class SectionNumbersTreeProcessor < Extensions::TreeProcessor
 
   def concat_sec_nr block, levels, dtype
     if block.numbered && (defined? block.level) && (!(levels) || block.level.to_i <= levels.to_i)
+      styles_arr = ( block.sectname == 'appendix' ? $styles_appendix_sntp : $styles_arr_sntp )
+      part_nums = ( block.sectname == 'appendix' ? false : $part_nums_sntp )
+      # Just in case levels_arr_sntp needs lengthening for this block:
       if block.level.to_i >= $levels_arr_sntp.length
         for i in ($levels_arr_sntp.length - 1)..block.level.to_i
           $levels_arr_sntp.push '0'
         end
       end
+      # Just in case someone has, in sequence, (wrongly) jumped two, rather than one level deeper
       if block.level.to_i > $level_previous_sntp + 1
         for i in ($level_previous_sntp + 1)..(block.level.to_i - 1)
-          $levels_arr_sntp[i] = $styles_arr_sntp[i]
+          $levels_arr_sntp[i] = styles_arr[i]
         end
       end
       $level_previous_sntp = block.level.to_i
-      # Get your block.number converted:
-      $levels_arr_sntp[block.level.to_i] = convert_block_nr block.number, block.level.to_i 
+      # Convert the block.number to the style you specified for this level; and store it in levels_arr_sntp[block.level.to_i] :
+      $levels_arr_sntp[block.level.to_i] = convert_block_nr block.number.to_s.tr('A-I', '1-9').to_i, block.level.to_i, styles_arr
       # Apply any appropriate prefix:
       pref = (dtype && dtype == 'book' && block.level.to_i == 1) ? $chap_nr_prefix_sntp : \
           ( (dtype && dtype == 'book' && block.level.to_i == 0) ? $part_nr_prefix_sntp : \
           ( block.level.to_i > 0 ? $sec_nr_prefix_sntp : ''))
+      pref = ( block.sectname == 'appendix' ? $appx_nr_prefix_sntp : pref )
       # When doctype='book', must check whether level-1 nrs belong in (the true) sec.nrs--or, level-0 nrs belong in the chap.nrs:
       if (dtype && dtype == 'book')
         if block.level.to_i > 1
@@ -72,6 +79,7 @@ class SectionNumbersTreeProcessor < Extensions::TreeProcessor
       else
         level_to_start = 1
       end
+      # Compose the number-string:
       str = pref + $levels_arr_sntp[level_to_start]
       for i in (level_to_start + 1)..block.level.to_i
         str += "#{$punct_sntp}#{$levels_arr_sntp[i]}"
@@ -79,14 +87,14 @@ class SectionNumbersTreeProcessor < Extensions::TreeProcessor
       str += $nr_suffix_sntp
       #  When doctype='book', must check whether chap-nrs are enabled--and, whether part-nrs are enabled--and what level ur at:
       dtype && dtype =='book'&& \
-         ((block.level.to_i == 1 && !$chap_nums_sntp) || (block.level.to_i == 0 && !$part_nums_sntp)) ? '' : str
+         ((block.level.to_i == 1 && !$chap_nums_sntp) || (block.level.to_i == 0 && !part_nums)) ? '' : str
     else
       nil
     end
   end
   
-  def convert_block_nr nr, level
-    my_char = String.new($styles_arr_sntp[level])
+  def convert_block_nr nr, level, styles_arr
+    my_char = String.new(styles_arr[level])
     if my_char == 'i' || my_char == 'I'
       big_roman = int_to_roman_str nr
       return (my_char == 'i' ? big_roman.downcase : big_roman)
