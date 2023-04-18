99e873b3283ebcbd54eab32449aaf91e011ba62d
Peter Kokot <peterkokot@gmail.com>
2023-04-19 00:06:06 +0200

    Fix ShellSessionTreeProcessor typos

--- lib/shell-session-treeprocessor.rb
+++ lib/shell-session-treeprocessor.rb
@@ -1,5 +1,5 @@
 RUBY_ENGINE == 'opal' ? (require 'shell-session-treeprocessor/extension') : (require_relative 'shell-session-treeprocessor/extension')
 
 Extensions.register do
-  treeprocessor ShellSessionTreeprocessor
+  treeprocessor ShellSessionTreeProcessor
 end
