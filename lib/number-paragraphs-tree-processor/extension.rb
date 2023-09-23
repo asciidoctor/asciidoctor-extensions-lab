b0f364943c665b2fb06b47137de04983aa6cde1b
Steve Simpson <ssimpson64@gmail.com>
2023-09-23 20:07:48 +1200

    replace original NumberParagraphsTreeProcessor with a more sophisticated version

--- lib/number-paragraphs-tree-processor/extension.rb
+++ lib/number-paragraphs-tree-processor/extension.rb
@@ -1,12 +1,28 @@
 class NumberParagraphsTreeProcessor < Asciidoctor::Extensions::TreeProcessor
   def process document
-    (document.find_by context: :paragraph).each_with_index do |p, idx|
-      pnum = idx + 1
-      # number paragraph using title
-      #p.title = %(#{pnum}.)
-
-      # or insert number into paragraph
-      p.lines.first.prepend %(#{pnum}. )
+    para_idx = 0
+    (document.find_by {|it| it.context == :section || it.context == :paragraph }).each do |block|
+      if block.context == :paragraph
+        next if block.lines.empty?
+        para_idx += 1
+        # uncomment whichever style you prefer
+        #add_paragraph_number_to_text block, para_idx
+        add_paragraph_number_to_title block, para_idx
+      else
+        para_idx = 0
+      end
     end
+  end
+
+  private
+
+  def add_paragraph_number_to_text para, num
+    para.lines[0] = %[#{num}) #{para.lines[0]}]
+    nil
+  end
+
+  def add_paragraph_number_to_title para, num
+    para.title = para.title? ? %[#{num}) #{para.instance_variable_get :@title}] : %[#{num})]
+    nil
   end
 end
