14c97895ac15cfb683f2d03ccfa5e0eba5c68608
Steve Simpson <ssimpson64@gmail.com>
2023-09-25 19:13:20 +1300

    Replace original NumberParagraphsTreeProcessor with the NumberSectionParagraphs variant.

--- lib/number-section-paragraphs.rb
+++
@@ -1,7 +0,0 @@
-require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
-
-RUBY_ENGINE == 'opal' ? (require 'number-section-paragraphs/extension') : (require_relative 'number-section-paragraphs/extension')
-
-Asciidoctor::Extensions.register do
-  tree_processor NumberSectionParagraphs
-end
