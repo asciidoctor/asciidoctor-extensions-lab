387c244c41b49e047b8eedc05b8f9a5b18048e5e
Dan Allen <dan.j.allen@gmail.com>
2023-09-25 02:11:49 -0600

    rewrite pre include processor to not rely on patched URI string

--- lib/pre-include-processor/extension.rb
+++ lib/pre-include-processor/extension.rb
@@ -1,3 +1,32 @@
+require 'tempfile'
+
+class PreIncludeProcessor < Asciidoctor::Extensions::IncludeProcessor
+  Processed = Module.new
+
+  def process doc, reader, target, attrs
+    attrlist = attrs.map {|k, v| %(#{k}="#{v}") }.join ','
+    resolved_target, _, _ = reader.send :resolve_include_path, target, attrlist, attrs
+    contents = ::File.readlines resolved_target, mode: 'r:UTF-8'
+    # ...now do whatever processing you want to do on the contents
+    #contents = contents.map {|l| l.sub %r/^# .*/, '' }
+    contents = contents.join
+    seed = [(File.basename target, '.*'), (File.extname target)]
+    ::Tempfile.create seed, (::File.dirname resolved_target), encoding: 'UTF-8', newline: :universal do |tmp_file|
+      tmp_target = (File.basename tmp_file.path).extend Processed
+      tmp_file.write contents
+      tmp_file.close
+      reader.unshift %(include::#{tmp_target}[#{attrlist}])
+      reader.send :preprocess_include_directive, tmp_target, attrlist
+    end
+    nil
+  end
+
+  def handles? target
+    !(Processed === target)
+  end
+end
+
+=begin
 URI.singleton_class.prepend (Module.new do
   def parse url
     return url if URI::Generic === url
@@ -27,10 +56,11 @@ end
 # requires -a allow-uri-read to be set
 class PreIncludeProcessor < Asciidoctor::Extensions::IncludeProcessor
   def process doc, reader, target, attributes
-    # NOTE do whatever processing you want to do on the contents
-    contents = (File.readlines target, mode: 'r:UTF-8').map {|l| l.sub %r/^# */, '' }.join
+    attrlist = attrs.map {|k, v| %(#{k}="#{v}") }.join ','
+    resolved_target, _, _ = reader.send :resolve_include_path, target, attrlist, attrs
+    contents = (File.readlines resolved_target, mode: 'r:UTF-8').map {|l| l.sub %r/^# */, '' }.join
+    # ...now do whatever other processing you want to do on the contents
     uri = URI::String.new target, contents
-    attrlist = attributes.map {|k, v| %(#{k}="#{v}") }.join ','
     reader.unshift %(include::#{target}[#{attrlist}])
     reader.send :preprocess_include_directive, uri, attrlist
     nil
@@ -40,3 +70,4 @@ class PreIncludeProcessor < Asciidoctor::Extensions::IncludeProcessor
     !(URI::String === target)
   end
 end
+=end
